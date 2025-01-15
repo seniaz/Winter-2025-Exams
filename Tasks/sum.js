66565d2f4e2c124b98c8f516f458a8b7e6436fa2
seniaz <kletsenkoov@gmail.com>
2025-01-16 00:14:26 +0200

    Deleted unnecessary code and improved previous steps

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -3,20 +3,14 @@
 
 'use strict'
 
-const sum (array) => {
-  const sum = [0];
-
+const sum = (array) => {
+  let res = 0;
   for (const element of array) {
-    const type = typeof element;
-    if (type === 'number') {
-      if (sum.length > 0) {
-        const elementSum = sum[sum.length - 1] + element;
-        sum.push(elementSum);
+    if (typeof element === 'number') {
+      res += element;
       }
     }
-  }
-  sum[0];
-  return sum[sum.length - 1];
+  return res;
 };
 
 module.exports = sum;
