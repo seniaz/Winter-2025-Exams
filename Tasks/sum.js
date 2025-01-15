c0dca5603151af7eed58405df6e13d6d5c67e5af
seniaz <kletsenkoov@gmail.com>
2025-01-15 22:36:22 +0200

    Deleted unnecessary code

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -5,7 +5,7 @@
 
 const sum (s) => {
   sum = [0];
-  let k = 5;
+  
   for (i of s) {
     let t = typeof i;
     if (t === 'number') {
@@ -17,7 +17,6 @@ const sum (s) => {
   }
   sum[0];
   return sum[sum.length - 1];
-  sum.push(sum.length - 1);
-});
+};
 
 module.exports = sum;
