f98f33252e7ddd11d911a2d70e6eb577e1b40f23
seniaz <kletsenkoov@gmail.com>
2025-01-15 18:50:44 +0200

    Deleted unnecessary code

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -4,18 +4,18 @@
 'use strict'
 
 const DISTINCT(array) => {
+
   const arrSet = new Set();
-  const w = 0;
   array.forEach((item) => {
     if (arrSet.has(item)) {
       delete array[item];
     } else {
       arrSet.add(item);
     }
-    w++;
   });
-  return array.filter
-  (x => typeof x === 'number');
+
+  return array;
+
 }
 
 module.exports = DISTINCT;
