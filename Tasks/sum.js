7fe890c1e3fa7b831bd836030b394da0d38ddff4
seniaz <kletsenkoov@gmail.com>
2025-01-15 23:03:13 +0200

    Added const for identifiers

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -4,10 +4,10 @@
 'use strict'
 
 const sum (s) => {
-  sum = [0];
-  
-  for (i of s) {
-    let t = typeof i;
+  const sum = [0];
+
+  for (const i of s) {
+    const t = typeof i;
     if (t === 'number') {
       if (sum.length > 0) {
         const new_Sum = sum[sum.length - 1] + i;
